3d920fad844b0b9140dbc6616f54414c2ef0fa7b
michelberger-a <alex.michelberger@gmail.com>
2024-09-13 14:31:54 -0400

    completed homework 2 for sql

--- 02_activities/homework/homework_2.sql
+++ 02_activities/homework/homework_2.sql
@@ -1,10 +1,19 @@
 --SELECT
 /* 1. Write a query that returns everything in the customer table. */
 
+-- return all the columns from customer  
+SELECT *  
+FROM customer as c  
 
 
 /* 2. Write a query that displays all of the columns and 10 rows from the cus- tomer table, 
 sorted by customer_last_name, then customer_first_ name. */
+
+-- all columns, with 10 rows from customer, sorted by last name then first name  
+SELECT *  
+FROM customer  
+ORDER BY customer_last_name, customer_first_name  
+LIMIT 10 
 
 
 
@@ -12,8 +21,15 @@ sorted by customer_last_name, then customer_first_ name. */
 --WHERE
 /* 1. Write a query that returns all customer purchases of product IDs 4 and 9. */
 -- option 1
+--return all customers purchases of product id 4 and 9  
+SELECT *  
+FROM customer_purchases  
+WHERE product_id IN (4,9) 
 
 -- option 2
+SELECT *  
+FROM customer_purchases  
+WHERE product_id = 4 OR product_id = 9
 
 /*2. Write a query that returns all customer purchases and a new calculated column 'price' (quantity * cost_to_customer_per_qty), 
 filtered by vendor IDs between 8 and 10 (inclusive) using either:
@@ -21,9 +37,17 @@ filtered by vendor IDs between 8 and 10 (inclusive) using either:
 	2.  one condition using BETWEEN
 */
 -- option 1
-
+-- all customer purchases and new column price (quantity * cost_to_customer_per_qty)  
+-- filter vendor ids for 8 to and including 10  
+SELECT *, (quantity * cost_to_customer_per_qty) as [price]  
+FROM customer_purchases  
+WHERE vendor_id BETWEEN 8 AND 10  
 
 -- option 2
+SELECT *, (quantity * cost_to_customer_per_qty) as [price]  
+FROM customer_purchases  
+WHERE vendor_id >= 8 AND vendor_id <= 10
+
 
 --CASE
 /* 1. Products can be sold by the individual unit or by bulk measures like lbs. or oz. 
@@ -31,12 +55,44 @@ Using the product table, write a query that outputs the product_id and product_n
 columns and add a column called prod_qty_type_condensed that displays the word “unit” 
 if the product_qty_type is “unit,” and otherwise displays the word “bulk.” */
 
+--output product_id and product_name, add column prod_qty_type_condensed and displays unit   
+-- if product_qty_type is unit or else display bulk  
+SELECT product_id, product_name,   
+	CASE WHEN product_qty_type = 'unit'  
+		THEN 'unit'  
+	ELSE 'bulk'  
+	END AS [prod_qty_type_condensed]  
+FROM product  
+
+
 
 /* 2. We want to flag all of the different types of pepper products that are sold at the market. 
 add a column to the previous query called pepper_flag that outputs a 1 if the product_name 
 contains the word “pepper” (regardless of capitalization), and otherwise outputs 0. */
 
 
+-- flag pepper products sold at market  
+-- add column to previous query "pepper_flag", 1 if product_name contains pepper  
+-- else, output 0  
+SELECT product_id, product_name,   
+	CASE WHEN product_qty_type = 'unit'  
+		THEN 'unit'  
+	ELSE 'bulk'  
+	END AS [prod_qty_type_condensed],  
+	CASE WHEN product_name LIKE '%pepper%'  
+		THEN 1  
+	ELSE 0  
+	END AS [pepper_flag]  
+FROM product 
+
 --JOIN
 /* 1. Write a query that INNER JOINs the vendor table to the vendor_booth_assignments table on the 
 vendor_id field they both have in common, and sorts the result by vendor_name, then market_date. */
+
+-- inner join vendor to vendor_booth_assignments on vendor_id  
+-- sort results by vendor_name and then market date  
+SELECT *  
+FROM vendor_booth_assignments AS vba  
+INNER JOIN vendor AS v  
+	ON vba.vendor_id = v.vendor_id  
+ORDER BY vendor_name, market_date
